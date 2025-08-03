6472c8a50d481b195337d22c530eee9fa118fb1d
Andre Derjagin <derjagin.andre@gmail.com>
2025-08-03 11:14:51 +0200

    feat: set up supabase + auth and login/signup page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import { login, signup } from "./actions";
+
+export default function LoginPage() {
+	return (
+		<form>
+			<label htmlFor="email">Email:</label>
+			<Input
+				id="email"
+				name="email"
+				type="email"
+				required
+			/>
+			<label htmlFor="password">Password:</label>
+			<Input
+				id="password"
+				name="password"
+				type="password"
+				required
+			/>
+			<div className="flex gap-2 w-fit pt-4">
+				<Button formAction={login}>Log in</Button>
+				<Button formAction={signup}>Sign up</Button>
+			</div>
+		</form>
+	);
+}
